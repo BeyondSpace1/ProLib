f52e36e66f73ad539adbf54d7c6709c9c14f41ca
Beyond.Space <78841805+BeyondSpace1@users.noreply.github.com>
2022-03-29 16:36:25 +0530

    Add files via upload

---
+++ views1/app1.js
@@ -0,0 +1,18 @@
+let btn1=document.querySelector('#m');
+let m1=document.querySelector('.menu');
+let bd=document.body;
+btn1.addEventListener('click',()=>{
+    m1.classList.toggle('c');
+    bd.classList.toggle('y');
+    if(m1.classList.contains('c')){
+        btn1.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
+        <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
+      </svg>`;
+    }
+    else{
+        btn1.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>`;
+    }
+});
